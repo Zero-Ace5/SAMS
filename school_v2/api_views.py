a0aa0a6e96b6e8c61bf1f89d89acdfbc08c3ae14
Zero-Ace5 <nitrovictory@outlook.com>
2025-12-28 22:56:18 +0530

    Optimizations...

--- school_v2/api_views.py
+++ school_v2/api_views.py
@@ -9,7 +9,7 @@ from django.shortcuts import get_object_or_404
 from .models import Student, Attendance
 from .serializers import StudentSerializer, AttendanceSerializer
 
-from django.db.models import Count, Q
+from django.db.models import Count, Q, F
 
 from django.core.cache import cache  # Added Cache
 
@@ -21,13 +21,17 @@ logger = logging.getLogger(__name__)
 
 class StudentListCreateAPI(APIView):
     def get(self, request):
-        cache_key = "student:list"
+        # added v1 meaning version 1. for tracking.
+        cache_key = "student:list:v1"
         data = cache.get(cache_key)
 
         if data is None:
             logger.warning("CACHE MISS → DB QUERY - 1")
-            students = Student.objects.all()
-            data = StudentSerializer(students, many=True).data
+            # students = Student.objects.all()
+            # data = StudentSerializer(students, many=True).data
+            students = Student.objects.values(
+                "id", "name", "roll_no", "s_class", "email")
+            data = list(students)
             cache.set(cache_key, data, 600)
         else:
             logger.warning("CACHE HIT - 1")
@@ -74,6 +78,16 @@ class AttendanceAPI(APIView):
         records = Attendance.objects.filter(date=date_param)
         serializer = AttendanceSerializer(records, many=True)
         return Response(serializer.data)
+
+        # records = (Attendance.objects.filter(date=date_param).annotate(
+        #     student_name=F("student__name"),
+        #     student_roll=F("student__roll_no"),).values(
+        #     "student_name",
+        #     "student_roll",
+        #     "present",
+        # ))
+
+        # return Response(list(records))
 
     def post(self, request):
         date_value = request.data.get("date")
